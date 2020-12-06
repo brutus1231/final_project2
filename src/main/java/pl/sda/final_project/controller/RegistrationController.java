49c56e43ad0fd5b985f7ad7f0b923d6f812c3855
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-12-06 11:36:42 +0100

    registration

--- src/main/java/pl/sda/final_project/controller/RegistrationController.java
+++ src/main/java/pl/sda/final_project/controller/RegistrationController.java
@@ -1,13 +1,16 @@
 package pl.sda.final_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.final_project.dto.UserDto;
 
 @Controller
 public class RegistrationController {
 
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(Model model) {
+        model.addAttribute("user", new UserDto());
         return "registration";
     }
 }
---
+++ src/main/java/pl/sda/final_project/dto/UserDto.java
@@ -0,0 +1,21 @@
+package pl.sda.final_project.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.sda.final_project.model.AccountStatus;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String username;
+    private String confirmedPassword;
+    private String password;
+    private String city;
+    private String address;
+    private Date createDate;
+    private AccountStatus status;
+}
