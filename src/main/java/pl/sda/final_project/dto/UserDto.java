caff98063e630952ae4d30f9a5dc3e222291c3f0
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-12-12 10:18:26 +0100

    validation when saving user

--- src/main/java/pl/sda/final_project/dto/UserDto.java
+++ src/main/java/pl/sda/final_project/dto/UserDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import pl.sda.final_project.model.AccountStatus;
 
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -11,8 +12,12 @@ import java.util.Date;
 public class UserDto {
 
     private Long id;
+
+    @Size(min = 5)
     private String username;
     private String confirmedPassword;
+
+    @Size(min = 5)
     private String password;
     private String city;
     private String address;
